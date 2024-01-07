80837db891ec895b94491beffb3434b3a964d1d6
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2024-01-07 20:44:42 +0200

    added an issue page for every created issue and some skeleton code whe n pages are loading

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,56 @@
-import { Button }  from '@radix-ui/themes'
-import Link from 'next/link'
-export default function Home() {
+import prisma from '@/prisma/client';
+import { Table } from '@radix-ui/themes';
+import IssueActions from './IssueActions';
+import IssueStatusBadge from '../components/IssueStatusBadge';
+import delay from 'delay';
+import Link from 'next/link';
+
+const IssuePage = async () => {
+  const issues = await prisma.issue.findMany();
+  await delay(2000);
+
   return (
-    <div>
-      <Button>
-        <Link href='/issues/new'>
-          NewIssue
-          </Link>
-      </Button>
+    <div className='max-w-[700px] w-[100%]' >
+      <IssueActions />
+
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>
+              Created At
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {issues.map(issue => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </Link>
+                <div className='block md:hidden'>
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+
+      </Table.Root >
+
     </div>
   )
 }
+
+export default IssuePage;
